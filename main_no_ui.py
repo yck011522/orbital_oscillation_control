155065308b9e11cb2b7b830293e2388cfa7d3161
Victor on Pi <yck011522@gmail.com>
2025-12-11 15:20:54 +0800

    Add main_no_ui.py for orbital oscillation control functionality

---
+++ main_no_ui.py
@@ -0,0 +1,41 @@
+import threading
+import time
+from pose_estimator import PoseEstimator
+from visualization_thread import PoseVisualizer
+from controller import Controller
+from sensor_utils import sensor_data_stream
+
+USE_LIVE_SYSTEM = True
+# USE_LIVE_SYSTEM = False
+
+if __name__ == "__main__":
+    # Start Pose Estimation
+    if USE_LIVE_SYSTEM:
+        pose_estimator = PoseEstimator(sensor_data_stream())
+    else:
+        pose_estimator = PoseEstimator("sensor_recording/glass02.csv")
+    pose_estimator.start()
+
+    # Start Controller
+
+    controller = Controller(pose_estimator, live_output=USE_LIVE_SYSTEM)
+    controller.start()
+
+    # Start Visualization Thread
+    # visualizer = PoseVisualizer(pose_estimator, controller)
+    # visualizer.start()
+
+    # Wait until everything is done
+    # controller.join()
+    # visualizer.join()
+
+    while True:
+        # print(f"Pose Frequency: {pose_estimator.freq_estimator.smoothed_freq :.2f} Hz, Controller Frequency: {controller.freq_estimator.smoothed_freq :.2f} Hz")
+        # print(f"Pose Estimator State: {pose_estimator.get_phase()}")
+        time.sleep(0.2)
+        print(f"speed : {pose_estimator.state.get('velocity',None)}")
+        # print(
+        #     f"Visualization Thread Frequency: {visualizer.freq_estimator.smoothed_freq:.2f} Hz, "
+        #     f"Controller Frequency: {controller.freq_estimator.smoothed_freq:.2f} Hz, "
+        #     f"Pose Estimator Frequency: {pose_estimator.freq_estimator.smoothed_freq:.2f} Hz"
+        # )
